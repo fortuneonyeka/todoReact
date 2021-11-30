f722e5e105e8a68b1d8408ade7aedadbed179999
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-30 10:01:42 +0100

    add input todo

---
+++ react-todo-app/src/components/InputTodo.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class InputTodo extends Component {
+  state = {
+    title: "",
+    fName: "",
+    lName: "",
+  };
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: "",
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add Todo..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        {/* <input
+          type="text"
+          name="fName"
+          placeholder="First Name..."
+          value={this.state.fName}
+          onChange={this.onChange}
+        /> */}
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
